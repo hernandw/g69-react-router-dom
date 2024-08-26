0e74e692ebc417cf4a24d3ff56e9c85e69b4e71a
Williams Hernández <hernandw@gmail.com>
2024-08-26 19:33:32 -0400

    :tada: estructura del proyecto de empresa DB

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-black navbar-dark">
+  <div className="container">
+    <a className="navbar-brand" href="/">Empresa DB</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ms-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="/about">about</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/login">Login</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/contact">Contact</a>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default NavBar
